e54e6324d4f46e799159410474aea129e934d473
hannesxc <chakravortyaditya@gmail.com>
2023-02-01 02:01:47 +0530

    .

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -35,14 +35,24 @@ function Welcome() {
               &ensp;Home
             </button>
           </Link>
-          <Link to={user ? '/resplash/home' : '/resplash/signin'}>
-            <button className="flex justify-center px-4 py-2 w-32 bg-blue-400/20 hover:bg-gray-400/30 rounded-3xl" type='button'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-              </svg>
-              &ensp;Sign In
-            </button>
-          </Link>
+          {user ?
+            <Link to='/resplash/home'>
+              <button className="flex justify-center px-4 py-2 w-36 bg-blue-400/20 hover:bg-gray-400/30 rounded-3xl" type='button'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                </svg>
+                &ensp;Signed In
+              </button>
+            </Link> :
+            <Link to='/resplash/signin'>
+              <button className="flex justify-center px-4 py-2 w-32 bg-blue-400/20 hover:bg-gray-400/30 rounded-3xl" type='button'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                </svg>
+                &ensp;Sign In
+              </button>
+            </Link>
+          }
         </div>
       </div>
     </div>
